d06753ba7416a3c11e067ea9fc6eb4c455f64b5b
Ward <ward.vandenbroeck@gluo.be>
2021-07-28 10:57:54 +0200

    deploy script

---
+++ deploy/deploy-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+aws cloudformation create-stack \
+	--stack-name "Magento-$RANDOM" \
+	--template-url "https://magento-quickstart-mod-c6926dad.s3.eu-west-1.amazonaws.com/github-magento-gluo/templates/magento-master.template" \
+	--capabilities CAPABILITY_IAM \
+	--disable-rollback \
+	--parameters file://parameters.json
