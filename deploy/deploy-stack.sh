3e335e59a6af40c5ae25de2a30482c95f8dbe7e2
Ward <ward.vandenbroeck@gluo.be>
2021-08-17 17:12:05 +0200

    integration elastic/rabbit into magento stack

--- deploy/deploy-stack.sh
+++ deploy/deploy-stack.sh
@@ -2,7 +2,7 @@
 
 aws cloudformation create-stack \
 	--stack-name "Magento-$RANDOM" \
-	--template-url "https://magento-quickstart-mod-c6926dad.s3.eu-west-1.amazonaws.com/github-magento-gluo/templates/magento-master.template" \
+	--template-url "https://magento-quickstart-mod-c6926dad.s3.eu-west-1.amazonaws.com/github-magento-gluo/templates/magento-master.template.yaml" \
 	--capabilities CAPABILITY_IAM \
 	--disable-rollback \
 	--parameters file://parameters.json
